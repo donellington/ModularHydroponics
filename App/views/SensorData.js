5a34004fdcbcc4de0b681e0adf06038862251b14
donellington <77545988+donellington@users.noreply.github.com>
2021-07-14 18:49:39 -0400

    Update SensorData.js
    Sensor data displays

--- App/views/SensorData.js
+++ App/views/SensorData.js
@@ -1,10 +1,33 @@
 // React Navigation Drawer with Sectioned Menu Options & Footer
 // https://aboutreact.com/navigation-drawer-sidebar-menu-with-sectioned-menu-options-footer/
 
-import * as React from 'react';
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import React, { useEffect, useState } from "react";
+// import styles from "../stylesheet";
+import { View, SafeAreaView, StyleSheet, Text, TextInput, Button, TouchableOpacity, ScrollView } from "react-native";
+
+// import * as React from 'react';
+// import {SafeAreaView, StyleSheet, View, Button, Text} from 'react-native';
 
 const SensorData = () => {
+
+  const [sdata, setSdata] = useState([]);
+
+  useEffect(() => {
+    fetch('https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/appwebsite-uirte/service/sensorData/incoming_webhook/sensorData')
+      .then((response) => response.json())
+      .then((data) => {
+        const data_array = [];
+        data_array[0] = data.sensors[0].temperature;
+        data_array[1] = data.sensors[0].humidity;
+        data_array[2] = data.sensors[0].waterlevel;
+        data_array[3] = data.sensors[0].lightvalue;
+        data_array[4] = data.sensors[0].tds;
+        data_array[5] = data.sensors[0].ph;
+        setSdata(data_array);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, padding: 16}}>
@@ -12,6 +35,11 @@ const SensorData = () => {
           <Text style={styles.textStyle}>
             Sensor Data 
             {'\n\n'}
+            {sdata[0]}
+            {sdata[1]}
+            {sdata[2]}
+            {sdata[3]}
+            {sdata[4]}
           </Text>
         <Text style={styles.footerHeading}>
           This page will host all important pod sensor data information
@@ -48,3 +76,4 @@ const styles = StyleSheet.create({
 });
 
 export default SensorData;
+
